a1795e0259be8c6c937b0aeeb5124ceb3a590e76
thuyanduong <thuyanannduong@gmail.com>
2020-07-14 18:32:18 -0400

    styling card

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ const Card = ({ villager, isFlipped, handleFlip }) => {
   return (
     <ReactCardFlip isFlipped={isFlipped} flipSpeedBackToFront={0.4} flipSpeedFrontToBack={0.4} >
       <div className="front-card" onClick={() => handleFlip(villager)}>
-        <Image src={process.env.PUBLIC_URL + `/assets/cards/clear_card.png`} alt="card"/>
+        <Image src={process.env.PUBLIC_URL + `/assets/cards/card.jpg`} alt="card"/>
       </div>
       <div className="game-card">
         <Image src={process.env.PUBLIC_URL + `/assets/villager_images/villager_${villager.villagerId}.png`} alt="card"/>
